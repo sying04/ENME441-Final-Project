0aba253031a39565a6e19d9f84f22a18b98b3a0d
Gurshaan-edu <gmann500@terpmail.umd.edu>
2025-11-24 16:11:55 -0500

    connect properly

--- laser_turret_code/jsonread.py
+++ laser_turret_code/jsonread.py
@@ -4,6 +4,7 @@ host = "http://192.168.1.254/"
 port = "8000"
 def read(host,port):
     c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    c.connect((host,port))
     data = c.recv(1024)
     return data
 
