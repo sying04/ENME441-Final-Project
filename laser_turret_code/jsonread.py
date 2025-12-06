69dd6f827198975ac4b2355dec613454741e8e26
Gurshaan-edu <gmann500@terpmail.umd.edu>
2025-12-05 19:45:56 -0500

    fixed json requesting

--- laser_turret_code/jsonread.py
+++ laser_turret_code/jsonread.py
@@ -1,11 +1,8 @@
-import socket
+import requests
 
 host = "http://192.168.1.254:8000/positions.json"
-port = 8000
-def read(host,port):
-    c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    c.connect((host,port))
-    data = c.recv(1024)
-    return data
 
-print(read(host,port))
+r = requests.get(host)
+data = r.json()
+
+print(data)
