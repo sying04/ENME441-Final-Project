625fdc64294dcdf984270dc9ba2a6e536927953d
Gurshaan-edu <gmann500@terpmail.umd.edu>
2025-12-08 16:49:37 -0500

    python classes suck

--- laser_turret_code/targeting.py
+++ laser_turret_code/targeting.py
@@ -2,12 +2,16 @@ import position_json_receiver
     
 
 class Targeter():
-    
+    position_receiver = 0
+    target_data_json = 0
+    target data = 0
     def __init__(self, host)
 	    self.position_receiver = position_json_receiver.PositionReceiver()
-        self.target_data = position_receiver.get_json()
+        self.target_data_json = position_receiver.get_json()
+        datas = target_data_json.dumps()
+        self.target_data = json.load(datas)
 
-    
+
 
 host = "http://192.168.1.254:8000/positions.json"
 
