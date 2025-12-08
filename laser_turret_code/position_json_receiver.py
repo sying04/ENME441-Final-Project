a34d1d717790cd510b0bda7f6a16f0898243ae8e
Gurshaan-edu <gmann500@terpmail.umd.edu>
2025-12-08 16:08:31 -0500

    more structured code
    targeting will be a class with all the math and json reading, it should run in its own thread along with the web stuff in the other.

--- laser_turret_code/position_json_receiver.py
+++ laser_turret_code/position_json_receiver.py
@@ -1,17 +1,21 @@
 import requests
 import json
 
-host = "http://192.168.1.254:8000/positions.json"
+
 class PositionReceiver():
 
     host = "http://192.168.1.254:8000/positions.json"
+    def __init__(self,json_host):
+        self.host = json_host
+
     
-    def get_json(host):
-        r = requests.get(host)
+    def get_json():
+        r = requests.get(self.host)
         return r.json()
 
 
 
 if __name__ == "__main__":
-    host = "http://127.0.0.1:8000/positions.json"
-    print(json.dumps(PositionReceiver.get_json(host),indent = 4))
+    host = "http://192.168.1.254:8000/positions.json"
+    json_receiver = PositionReceiver(host)
+    print(json.dumps(json_receiver.get_json(),indent = 4))
---
+++ laser_turret_code/targeting.py
@@ -0,0 +1,14 @@
+import position_json_receiver
+    
+
+class Targeter():
+    
+    def __init__(self, host)
+	    self.position_receiver = position_json_receiver.PositionReceiver()
+        self.target_data = position_receiver.get_json()
+
+    
+
+host = "http://192.168.1.254:8000/positions.json"
+
+enme441_targeter = Targeter(host)
